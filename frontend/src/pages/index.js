78ec81aa0a2db604bb8a326b6631aff5c321d419
Thomas Sweeney <skywriter@gmail.com>
2021-06-05 18:59:51 +0100

    Update index.js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -31,6 +31,7 @@ const Index = ({ data }) => {
             name: "description",
             content:
               "Love feeling good? At Fitness Republic, we promote health and wellbeing over body image. Get started training with Ashby's longest running gym today"
+          },
           {
             name: "keywords",
             content:
