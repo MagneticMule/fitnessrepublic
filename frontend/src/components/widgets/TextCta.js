ceaa909a4c6c05c98c92ec8a33a23433ec8eb255
Tommy <skywriter@gmail.com>
2021-06-17 11:07:50 +0100

    (feat) added thank you page for seven day pass signup

--- frontend/src/components/widgets/TextCta.js
+++ frontend/src/components/widgets/TextCta.js
@@ -2,20 +2,30 @@ import React from 'react';
 import {Link} from 'gatsby';
 import styled from 'styled-components';
 import { FaPhoneSquareAlt } from "react-icons/fa";
+import { device } from "../../styles/DeviceSizes";
 
 const Cta = styled.aside`
   display: flex;
   flex-direction: column;
 
-  padding:0;
-  margin:0;
+  padding: 0;
+  margin: 0;
   > {
-    padding:0;
-    margin:0;
-    font-weight:bold;
+    padding: 0;
+    margin: 0;
+    font-weight: bold;
     color: var(--deeper-purple);
   }
 
+  @media ${device.mobileM} {
+    grid-column: 1/5;
+    margin-bottom: 2em;
+  }
+
+  @media ${device.laptop} {
+    grid-column: ${(props) => (props.pos ? props.pos : "1/2")};
+    margin-bottom: 0;
+  }
 `;
 
 const TextCta = ({introText, link, linkText}) => {
