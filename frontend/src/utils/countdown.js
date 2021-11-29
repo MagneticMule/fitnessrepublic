38567834da5fa9f203ebbd0bd348f09a66344775
Tommy <skywriter@gmail.com>
2021-11-29 00:13:24 +0000

    fix(countdown) added default return for parts object if countdown is !>0

--- frontend/src/utils/countdown.js
+++ frontend/src/utils/countdown.js
@@ -4,15 +4,25 @@ let countDownTimer = (td) => {
   const currentDate = +new Date();
   let difference = targetDate - currentDate;
   if (difference > 0) {
-    console.log(difference);
     const parts = {
       days: Math.floor(difference / (1000 * 60 * 60 * 24)),
       hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
       minutes: Math.floor((difference / 1000 / 60) % 60),
       seconds: Math.floor((difference / 1000) % 60),
     };
+
+  } else {
+    const parts = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
     return parts;
   }
-};
+
+}
+
+
 
 export default countDownTimer;
